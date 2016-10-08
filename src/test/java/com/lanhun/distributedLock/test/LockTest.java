b991a413b310512da0f23d74ba1f09eaf72d0215
蓝魂 <445052471@qq.com>
2016-10-08 11:49:37 +0800

    测试用例修改

--- src/test/java/com/lanhun/distributedLock/test/LockTest.java
+++ src/test/java/com/lanhun/distributedLock/test/LockTest.java
@@ -3,6 +3,10 @@ package com.lanhun.distributedLock.test;
 import com.lanhun.distributedLock.DistributeLock;
 import com.lanhun.distributedLock.Lock;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisSentinelPool;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class LockTest {
 
@@ -10,20 +14,26 @@ public class LockTest {
         String type = "test";
         String host = "127.0.0.1";
         int port = 6379;
+        String master="mymaster";
+        String password="ffzx6102";
+        Set<String> x=new HashSet<>();
+        x.add("192.168.2.195:26379");
 
-        JedisPool jedisPool = new JedisPool(host, port);
+       // JedisPool jedisPool = new JedisPool(host, port);
+        JedisSentinelPool jedisPool= new JedisSentinelPool(master,x,password);
         Lock lock = new DistributeLock();
         ((DistributeLock) lock).setJedisPool(jedisPool);
-        // lock.lock(type, "");
+         lock.lock(type, "");
 
 
         //lock.lock(type+"12");
 
         //lock.unLock(type+"12");
         //lock.unLock(type);
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1; i++) {
             t(type, lock);
         }
+        lock.unLock(type,"");
         System.out.println("end");
     }
 
@@ -31,9 +41,9 @@ public class LockTest {
         boolean x = false;
         long t = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
-            x = lock.lock(type, "");
-            System.out.println(x);
-            lock.unLock(type, "");
+            x = lock.lock(type, i+"");
+           // System.out.println(x);
+           // lock.unLock(type, "");
         }
         t = System.currentTimeMillis() - t;
         System.out.println(t + " ms");
